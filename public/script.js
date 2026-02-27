cf72903f594d3d418274cb06c76940b3fcd284b8
Luke Evanson <luke.p.evanson@icloud.com>
2026-02-26 17:53:00 -0800

    skib

--- public/script.js
+++ public/script.js
@@ -4,12 +4,13 @@ import { getFirestore, doc, onSnapshot, setDoc, getDoc } from "https://www.gstat
 import { initCanvas, initParallax } from "./ui.js";
 
 const app = initializeApp({
-  apiKey:"__FIREBASE_API_KEY__",
-  authDomain:"__FIREBASE_AUTH_DOMAIN__",
-  projectId:"__FIREBASE_PROJECT_ID__",
-  storageBucket:"__FIREBASE_STORAGE_BUCKET__",
-  messagingSenderId:"__FIREBASE_MESSAGING_SENDER_ID__",
-  appId:"__FIREBASE_APP_ID__"
+  apiKey: "__FIREBASE_API_KEY__",
+  authDomain: "__FIREBASE_AUTH_DOMAIN__",
+  projectId: "__FIREBASE_PROJECT_ID__",
+  storageBucket: "__FIREBASE_STORAGE_BUCKET__",
+  messagingSenderId: "__FIREBASE_MESSAGING_SENDER_ID__",
+  appId: "__FIREBASE_APP_ID__",
+  measurementId: "G-BCDM484BXD"
 });
 const db = getFirestore(app);
 const REFS = {
